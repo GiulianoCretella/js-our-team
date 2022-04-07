9e1c4d3049452fd00d1df6fb46c1629038e57907
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-07 15:12:24 +0200

    array di oggetti creato con nomi componenti

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
+
+// creo array nel quale inserisco gli oggetti in questione(membri del team)
+let teamArray = [
+    {
+        immagine : 'wayne-barnett-founder-ceo.jpg',
+        nome : 'Wayne Barnett',
+        jobTitle : 'Founder & CEO'
+    },
+    {
+        immagine : 'angela-caroll-chief-editor.jpg',
+        nome : 'Angela Carrol',
+        jobTitle : 'Chief Editor'
+    },
+    {
+        immagine : 'walter-gordon-office-manager.jpg',
+        nome : 'Walter Gordon',
+        jobTitle : 'Office Manager'
+    },
+    {
+        immagine : 'angela-lopez-social-media-manager.jpg',
+        nome : 'Angela Lopez',
+        jobTitle : 'Social Media Manager'
+    },
+    {
+        immagine : 'scott-estrada-developer.jpg',
+        nome : 'Scott Estrada',
+        jobTitle : 'Developer'
+    },
+    {
+        immagine : 'barbara-ramos-graphic-designer.jpg',
+        nome : 'Barbara Ramos',
+        jobTitle : 'Graphic Designer'
+    }
+];
